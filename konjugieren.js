4b10e795a3f57af01876e6e17a87799dbad00ded
David Mendoza <dmendozasommer@gmail.com>
2023-12-18 14:20:48 +0100

    html2 add, waiting for setup up

--- konjugieren.js
+++ konjugieren.js
@@ -1,14 +1,23 @@
+window.onload = function () {
+  counter();
+};
+
 const conjugatedVerbs = document.getElementById("conjugated_verbs");
 const containerBgFilter = document.getElementById("container_bg_filter");
 const container = document.getElementById("container");
 const check = document.getElementById("sichern_btn");
-const printBtn = document.getElementById("print_btn");
+const counterBubble = document.getElementById("counter_bubble");
+
 let verbList = [];
 
-document.getElementById("counter_bubble").innerHTML = document.querySelectorAll(
-  "#conjugated_verbs .boxbox"
-).length;
+// Counter for the number of conjugated verbs
+function counter() {
+  counterBubble.innerHTML = document.querySelectorAll(
+    "#conjugated_verbs .boxbox"
+  ).length;
+}
 
+// Listing conjugated verbs
 check.addEventListener("click", checked);
 function checked() {
   let verbConjugations = [];
@@ -18,7 +27,6 @@ function checked() {
     verbConjugations.push(rows[i].lastChild.innerText);
   }
   verbList.push(verbConjugations);
-  console.log(verbList);
 
   let verbBox = `<div id="${verbConjugations[0]}_verb_box" class="boxbox">
     <h1>${verbConjugations[0]}</h1>
@@ -28,14 +36,13 @@ function checked() {
     <div class=verb_row><p class="pronoun_cell">wir</p><p class="verb_cell">${verbConjugations[4]}</p></div>
     <div class=verb_row><p class="pronoun_cell">ihr</p><p class="verb_cell">${verbConjugations[5]}</p></div>
     <div class=verb_row><p class="pronoun_cell">sie/Sie</p><p class="verb_cell">${verbConjugations[6]}</p></div>
-    <span class="material-symbols-outlined"> delete </span>
+    <span class="material-symbols-outlined delete_btn_in_box" onclick="logClickedElement(event)">delete</span>
   </div>`;
   conjugatedVerbs.innerHTML += verbBox;
-
-  document.getElementById("counter_bubble").innerHTML =
-    document.querySelectorAll("#conjugated_verbs .boxbox").length;
+  counter();
 }
 
+// Scrolling opacity effect
 window.addEventListener("scroll", scrollBgOpacity);
 function scrollBgOpacity() {
   const opacity = Math.min(0.6, window.scrollY / 900);
@@ -44,6 +51,8 @@ function scrollBgOpacity() {
   }
 }
 
-function deleteBtn() {
-  this.parentNode.remove();
+function logClickedElement(event) {
+  var clickedElement = event.target;
+  clickedElement.parentElement.remove();
+  counter();
 }
