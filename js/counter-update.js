4f86566e8101036756cd3b1ce31adff01b2947ee
David Mendoza <dmendozasommer@gmail.com>
2023-12-19 23:38:07 +0100

    Done without drucken btn

--- js/counter-update.js
+++ js/counter-update.js
@@ -5,7 +5,6 @@ sichernBtn.addEventListener("click", counterUpdateAnimation);
 
 function counterUpdateAnimation() {
   counterBtn.classList.add("bnw_animation");
-  counterBtn.style.transform = "rotate(15deg)";
   setTimeout(function () {
     counterBtn.classList.remove("bnw_animation");
   }, 1000);
--- js/konjugieren.js
+++ js/konjugieren.js
@@ -7,28 +7,48 @@ const containerBgFilter = document.getElementById("container_bg_filter");
 const container = document.getElementById("container");
 const check = document.getElementById("sichern_btn");
 const counterBubble = document.getElementById("counter_bubble");
-
+const conjugationBox = document.getElementById("conjugation_box");
 let verbList = [];
-
-// Counter for the number of conjugated verbs
-function counter() {
-  counterBubble.innerHTML = document.querySelectorAll(
-    "#conjugated_verbs .boxbox"
-  ).length;
-}
 
 // Listing conjugated verbs
 check.addEventListener("click", checked);
 function checked() {
+  // Variable storing the value of the input field to conjugate.
   let verbConjugations = [];
   verbConjugations.push(document.getElementById("verb_input").value);
+  console.log(verbConjugations[0]);
+
+  // Selecting "table" which has the current conjugated verb and its 6 items +
+  // item 0 containing the inputed verb or first value.
   const rows = document.getElementsByTagName("table")[0].rows;
+
+  // Pushing the 6 conjugated verbs to the verbConjugations array.
   for (let i = 0; i < rows.length; i++) {
     verbConjugations.push(rows[i].lastChild.innerText);
   }
+  console.log(verbConjugations);
   verbList.push(verbConjugations);
 
-  let verbBox = `<div id="${verbConjugations[0]}_verb_box" class="boxbox">
+  var alreadyConjugated = false;
+
+  // Pushing new array into html list container if the value is not already
+  // conjugated
+  for (const child of conjugatedVerbs.children) {
+    if (child.id === verbConjugations[0]) {
+      alreadyConjugated = true;
+      break;
+    }
+  }
+
+  if (alreadyConjugated) {
+    conjugationBox.style.backgroundColor = "rgb(255 74 74 / 84%)";
+    check.innerText = "Schon gespeichert";
+    setTimeout(function () {
+      check.innerText = "Speichern";
+      conjugationBox.style.backgroundColor = "rgba(0, 0, 0, 0.84)";
+    }, 1000);
+  } else {
+    let verbBox = `<div id="${verbConjugations[0]}" class="boxbox">
     <h1>${verbConjugations[0]}</h1>
     <div class=verb_row><p class="pronoun_cell">ich</p><p class="verb_cell">${verbConjugations[1]}</p></div>
     <div class=verb_row><p class="pronoun_cell">du</p><p class="verb_cell">${verbConjugations[2]}</p></div>
@@ -37,9 +57,11 @@ function checked() {
     <div class=verb_row><p class="pronoun_cell">ihr</p><p class="verb_cell">${verbConjugations[5]}</p></div>
     <div class=verb_row><p class="pronoun_cell">sie/Sie</p><p class="verb_cell">${verbConjugations[6]}</p></div>
     <span class="material-symbols-outlined delete_btn_in_box" onclick="logClickedElement(event)">delete</span>
-  </div>`;
-  conjugatedVerbs.innerHTML += verbBox;
-  counter();
+    </div>`;
+    conjugatedVerbs.innerHTML += verbBox;
+    // Add to counter
+    counter();
+  }
 }
 
 // Scrolling opacity effect
@@ -51,8 +73,16 @@ function scrollBgOpacity() {
   }
 }
 
+// Deleting conjugated verbs
 function logClickedElement(event) {
   var clickedElement = event.target;
   clickedElement.parentElement.remove();
   counter();
+}
+
+// Counter for the number of conjugated verbs
+function counter() {
+  counterBubble.innerHTML = document.querySelectorAll(
+    "#conjugated_verbs .boxbox"
+  ).length;
 }
